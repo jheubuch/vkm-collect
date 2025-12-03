ec39ce1a3484c2c7959669cf56a44de196c32b54
Jonas Heubuch <jonas.heubuch@outlook.com>
2025-12-03 08:45:47 +0100

    :sparkles: Set theme color to current primary

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Meta } from '@angular/platform-browser';
+import PwaService from './core/pwa/pwa';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,30 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  private readonly meta = inject(Meta);
+  private readonly pwaService = inject(PwaService);
+
+  constructor() {
+    effect(() => {
+      const darkMode = this.pwaService.darkMode();
+      const primary = this.getPrimaryColor(darkMode);
+      const existing = this.meta.getTag("name='theme-color'");
+      if (existing) {
+        this.meta.updateTag({ name: 'theme-color', content: primary }, 'name="theme-color"');
+      } else {
+        this.meta.addTag({ name: 'theme-color', content: primary });
+      }
+    });
+  }
+
+  private getPrimaryColor(dark: boolean): string {
+    const primaryColor = getComputedStyle(document.documentElement).getPropertyValue(
+      '--mat-sys-primary'
+    );
+
+    const regexMatches = primaryColor.match(/light-dark\(\s*([^,]+)\s*,\s*([^)]+)\s*\)/);
+
+    return regexMatches !== null ? regexMatches[dark ? 2 : 1] : '';
+  }
+}
--- src/app/core/pwa/pwa.ts
+++ src/app/core/pwa/pwa.ts
@@ -1,4 +1,4 @@
-import { computed, inject, Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { SwUpdate } from '@angular/service-worker';
 
@@ -7,12 +7,23 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export default class PwaService {
   private swUpdate = inject(SwUpdate);
+
   private versionUpdates = toSignal(this.swUpdate.versionUpdates);
   public updateAvailable = computed(() => {
     const updateState = this.versionUpdates()?.type;
     console.log('PWA update state', updateState);
     return updateState === 'VERSION_READY';
   });
+
+  private readonly mediaMatcher = matchMedia('(prefers-color-scheme: dark)');
+  private readonly _darkMode = signal<boolean>(this.mediaMatcher.matches);
+  public readonly darkMode = this._darkMode.asReadonly();
+
+  constructor() {
+    this.mediaMatcher.addEventListener('change', (event) => {
+      this._darkMode.set(event.matches);
+    });
+  }
 
   public async activateUpdate(): Promise<void> {
     if (this.updateAvailable()) {
